977876b64f8c3fbcf1ad06655e6a3e180bdfc710
ksh vs <kshman@msn.com>
2023-03-06 03:26:03 +0900

    DropContent -> BsDropContent

--- Blazor.Bootstrap/BsDropMenu.cs
+++ Blazor.Bootstrap/BsDropMenu.cs
@@ -3,7 +3,7 @@
 /// <summary>드랍 콘텐트</summary>
 /// <remarks>원래 <see cref="BsDropDown"/> 아래 콘텐트 구성용이지만, 단독으로 쓸 수 있음</remarks>
 /// <seealso cref="BsDropMenu"/>
-public class DropContent : BsDropMenu
+public class BsDropContent : BsDropMenu
 {
 	//
 	protected override string TagName => "div";
